0a3b2d70dc815e362553b1b90bb874b0f3288ce7
NoohAlavi <59208101+NoohAlavi@users.noreply.github.com>
2022-11-11 08:37:17 -0500

    Update NoohAlaviAssignment14.java

--- NoohAlaviAssignment14/src/noohalaviassignment14/NoohAlaviAssignment14.java
+++ NoohAlaviAssignment14/src/noohalaviassignment14/NoohAlaviAssignment14.java
@@ -17,21 +17,27 @@ public class NoohAlaviAssignment14 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        // Set variables
         Scanner keyedInput = new Scanner(System.in);
         final String INPUT_PROMPT = ">>> ";
+        
         String choice;
+        
         int number;
         int power;
         int answer;
         
+        // Program main loop
         while (true) {
+            // Display menu
             System.out.println("1… Find the value of a number squared (5^2 = 5 x 5)");
             System.out.println("2… Find the value of a number cubed (7^3 = 7 x 7 x 7)");
             System.out.println("3… Find the value of a number, to any power (2^5 = 2 x 2 x 2 x 2 x 2)");
             System.out.println("4… Exit\n");
         
+            // Get mode
             System.out.print(INPUT_PROMPT);
-            choice = keyedInput.nextLine();
+            choice = keyedInput.next();
         
             switch (choice) {
                 case "1":
@@ -39,29 +45,43 @@ public class NoohAlaviAssignment14 {
                     System.out.print(INPUT_PROMPT);
                     
                     number = keyedInput.nextInt();
-                    answer = number * number;
+                    answer = number * number; // square number
                     
-                    System.out.println("\n" + number + " squared is equal to " + answer + ".");
+                    System.out.println("\n" + number + "^2 = " + answer); // print answer
                     break;
                 case "2":
                     System.out.println("\nEnter a number: ");
                     System.out.print(INPUT_PROMPT);
                     
                     number = keyedInput.nextInt();
-                    answer = number * number * number;
+                    answer = number * number * number; //cube number
                     
-                    System.out.println("\n" + number + " cubed is equal to " + answer + ".");
+                    System.out.println("\n" + number + "^3 = " + answer); // print answer
                     break;
                 case "3":
+                    System.out.println("\nEnter a number: ");
+                    System.out.print(INPUT_PROMPT);
+                    number = keyedInput.nextInt();
+                    
+                    System.out.println("\nEnter a power: ");
+                    System.out.print(INPUT_PROMPT);
+                    power = keyedInput.nextInt();
+                    
+                    answer = number; //initial value, will be updated
+                    for (int i = 0; i < (power - 1); i++) { // loop through appropriate amount of times
+                        answer *= number;
+                    }
+                
+                    System.out.println("\n" + number + "^" + power + " = " + answer); // print answer
                     break;
                 case "4":
-                    System.out.println("\nThank you for using the program! :D");
+                    System.out.println("\nThank you for using the program!");
                     return; // Exit program
                 default:
-                    System.out.println("Please enter a number in between 1 and 4!");
+                    System.out.println("Please enter an integer in between 1 and 4!"); // print answer
                     break; // Go through loop again
             }
-            System.out.println("------------------------------------------------");
+            System.out.println("<>------------------------------------------------<>\n\n");
         }
     }
 }
