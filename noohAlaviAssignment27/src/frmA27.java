7286769faae7752e83dc75f627d03ec2ccc3a473
Nooh Alavi <noohalavi@pop-os.localdomain>
2023-01-29 21:57:52 -0500

    Added comment

--- noohAlaviAssignment27/src/frmA27.java
+++ noohAlaviAssignment27/src/frmA27.java
@@ -14,6 +14,7 @@ import java.util.Collections;
  */
 public class frmA27 extends javax.swing.JFrame {
 
+    // Parallel ArrayLists
     ArrayList<String> carNames = new ArrayList<>();
     ArrayList<String> carSpeeds = new ArrayList<>();
     ArrayList<String> animalNames = new ArrayList<>();
